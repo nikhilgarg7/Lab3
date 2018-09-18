65a90c6919b3c56b312c8876464d9e1ab2a77309
nikhilgarg7 <nikhilg4@illinois.edu>
2018-09-18 18:28:06 -0500

    WordCount method added.

--- src/WebScraper.java
+++ src/WebScraper.java
@@ -1,4 +1,49 @@
+import java.io.IOException;
+import java.net.URL;
+import java.util.Scanner;
+
 public class WebScraper {
 
+    public static void main(String[] args) {
+        System.out.println(urlToString("http://erdani.com/tdpl/hamlet.txt"));
+        System.out.println(wordCount(urlToString("http://erdani.com/tdpl/hamlet.txt")));
+        System.out.println(urlToString("https://www.bls.gov/tus/charts/chart9.txt"));
+    }
+    /**
+     * Retrieve contents from a URL and return them as a string.
+     *
+     * @param url url to retrieve contents from
+     * @return the contents from the url as a string, or an empty string on error
+     */
 
+    public static String urlToString(final String url) {
+        Scanner urlScanner;
+        try {
+            urlScanner = new Scanner(new URL(url).openStream(), "UTF-8");
+        } catch (IOException e) {
+            return "";
+        }
+        String contents = urlScanner.useDelimiter("\\A").next();
+
+        urlScanner.close();
+        return contents;
+    }
+
+    public static int wordCount (String contents) {
+        int wordCount = 0;
+        for (int i = 0; i < contents.length(); i++) {
+            String newWord = "";
+            while ((int) contents.charAt(i) != (int) ' ') {
+                if (i == contents.length() - 1) {
+                    break;
+                }
+                newWord = newWord + contents.charAt(i);
+                i++;
+            }
+            if (newWord.equalsIgnoreCase("prince")) {
+                wordCount++;
+            }
+        }
+        return wordCount;
+    }
 }
